f6cba72d6dfa9c9d7c11a76a5d7707ca4ddd8769
Manjesh Malavalli <manjesh.malavalli@apps.ask.com>
2016-07-05 21:22:37 -0700

    WebRTC demo

---
+++ projects/WebRTC/scripts/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by malavallim on 7/5/16.
+ */
+(function () {
+  var getUserMedia = navigator.webkitGetUserMedia || function () {
+    console.log('getUserMedia() not supported.');
+  };
+  var constraints = {
+    audio: false,
+    video: true
+  };
+  var setVideoSource = function (video, stream) {
+    video.src = window.URL && window.URL.createObjectURL ? window.URL.createObjectURL(stream) : stream;
+  };
+  var gotMedia = function (stream) {
+    console.log('getUserMedia(): Got stream - ', stream);
+    setVideoSource(document.getElementById('self'), stream);
+  };
+  var gotError = function (error) {
+    console.log('getUserMedia(): - ', error);
+  };
+
+  getUserMedia.call(navigator, constraints, gotMedia, gotError);
+})();
