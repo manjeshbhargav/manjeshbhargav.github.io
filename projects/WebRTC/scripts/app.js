ddd5f0f8adf23356d6fd858e4fbebfe44a3a8e43
Manjesh Malavalli <manjesh.malavalli@apps.ask.com>
2016-07-06 23:30:48 -0700

    WebRTC demo

--- projects/WebRTC/scripts/app.js
+++ projects/WebRTC/scripts/app.js
@@ -7,6 +7,65 @@ var sigChannelOther = new LoopbackSignalingChannel();
 
 (function () {
 
+  var video = document.getElementById('other');
+  var pc = new RTCPeerConnection(null, { optional: [] });
+
+  pc.onaddstream =
+    function (e) {
+      console.log('Other: adding self\'s stream - ', e.stream);
+      video.srcObject = e.stream;
+    };
+
+  pc.onicecandidate =
+    function (e) {
+      if (e.candidate) {
+        console.log('Other: sending ice candidate - ', e.candidate.candidate);
+        sigChannelSelf.send({ candidate: e.candidate.candidate });
+      }
+    };
+
+  sigChannelOther.receive(
+    function (data) {
+      if (data.hasOwnProperty('offer')) {
+        console.log('Other: offer - ', data.offer);
+        pc.setRemoteDescription(new RTCSessionDescription(data.offer))
+          .then(
+            function () {
+              return pc.createAnswer();
+            }
+          ).then(
+            function (answer) {
+              console.log('Other: answer - ', answer.sdp);
+              pc.setLocalDescription(answer)
+                .then(
+                  function () {
+                    console.log('Other: local sdp set, sending answer...');
+                    sigChannelSelf.send({
+                      answer: answer.sdp
+                    });
+                  }
+                ).catch(
+                  console.log.bind(console)
+                );
+            }
+          ).catch(
+            console.log.bind(console)
+          );
+      }
+      else if (data.hasOwnProperty('candidate')) {
+        console.log('Other: receiving ice candidate - ', data.candidate);
+        pc.addIceCandidate(new RTCIceCandidate(data.candidate))
+          .catch(
+            console.log.bind(console)
+          );
+      }
+    }
+  );
+
+})();
+
+(function () {
+
   var video = document.getElementById('self');
   var pc = new RTCPeerConnection(null, { optional: [] });
 
@@ -68,89 +127,5 @@ var sigChannelOther = new LoopbackSignalingChannel();
   ).catch(
     console.log.bind(console)
   );
-})();
 
-//
-//(function () {
-//
-//  var selfVideo = document.getElementById('self');
-//  var remoteVideo = document.getElementById('remote');
-//
-//  var pcServers = null;
-//  var pcConstraints = { optional: [] };
-//  var pcSelf = new RTCPeerConnection(pcServers, pcConstraints);
-//  var pcOther = new RTCPeerConnection(pcServers, pcConstraints);
-//  var sigChannelSelf = new LoopbackSignalingChannel();
-//  var sigChannelOther = new LoopbackSignalingChannel();
-//
-//  var mediaConstraints = {
-//    audio: false,
-//    video: {
-//      width: { ideal: 320 },
-//      height: { ideal: 240 }
-//    }
-//  };
-//
-//  var generateOffer = function (stream) {
-//    console.log('Self: Got stream - ', stream);
-//    selfVideo.srcObject = stream;
-//    pcSelf.addStream(stream);
-//    pcSelf.onicecandidate = function (e) {
-//      console.log('Self: Sending candidate')
-//      sigChannelOther.send({
-//        command: 'addIceCandidate',
-//        candidate: e.candidate.toJSON()
-//      });
-//    };
-//    return pcSelf.createOffer();
-//  };
-//
-//  var sendOffer = function (sdp) {
-//    pcSelf.setLocalDescription(sdp)
-//      .then(function () {
-//        sigChannelOther.send({
-//          command: 'setRemoteDescription',
-//          sdp: sdp.toJSON()
-//        });
-//      });
-//  };
-//
-//  var logError = function (error) {
-//    console.log('getUserMedia(): - ', error);
-//  };
-//
-//  sigChannelOther.receive(function (data) {
-//    switch (data.command) {
-//      case 'addIceCandidate': {
-//        pcOther.addIceCandidate(new RTCIceCandidate(data.candidate.candidate));
-//        break;
-//      }
-//      case 'setRemoteDescription': {
-//        pcOther.setRemoteDescription(new RTCSessionDescription(data.sdp.sdp))
-//          .then(function () {
-//            return pcOther.createAnswer();
-//          }).then(function (sdp) {
-//            pcOther.setLocalDescription(sdp)
-//              .then(function () {
-//                sigChannelSelf.send({
-//                  command: 'setRemoteDescription',
-//                  sdp: sdp.toJSON()
-//                });
-//              });
-//          });
-//      }
-//    }
-//  });
-//
-//  sigChannelSelf.receive(function (data) {
-//    pcSelf.setRemoteDescription(new RTCSessionDescription(data.sdp.sdp))
-//      .then(function () {
-//        console.log('Connection established.');
-//      });
-//  });
-//
-//  navigator.mediaDevices.getUserMedia(mediaConstraints)
-//    .then(generateOffer)
-//    .then(sendOffer)
-//    .catch(logError);
-//})();
+})();
